ac833633817ba937aaf14ea0332be90c2675592e
RobertoGzzMtz <robertogzzmtz@gmail.com>
2022-06-29 10:18:41 -0500

    updated url

--- src/App.js
+++ src/App.js
@@ -71,7 +71,7 @@ class App extends Component {
       },
     };
     this.setState({ authLoading: true });
-    fetch(`https://fakebook-demo-rest-ap.herokuapp.com/graphql`, {
+    fetch(`https://fakebook-demo-rest-api.herokuapp.com/graphql`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -134,7 +134,7 @@ class App extends Component {
         password: authData.signupForm.password.value,
       },
     };
-    fetch(`https://fakebook-demo-rest-ap.herokuapp.com/graphql`, {
+    fetch(`https://fakebook-demo-rest-api.herokuapp.com/graphql`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
